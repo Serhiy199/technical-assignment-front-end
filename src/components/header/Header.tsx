6cc17311f1cb018c633b17292145531abe5acb81
Serhiy Horodetskiy <bryklin.best.1994@gmail.com>
2025-02-28 08:06:12 +0200

    прибрав помилки з header щоб задеплоїти

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import css from './header.module.css';
+import css from './Header.module.css';
 import '../../index.css';
 import Logo from '../logo/logo';
 import Navigation from './Navigation/Navigation';
